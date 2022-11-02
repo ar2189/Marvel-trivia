d24b8ebd30e4663ca063cdeab2dcc6e49536907b
ar2189 <atif2189.ar@gmail.com>
2022-11-02 23:29:00 +0000

    Add code to show question inside the container box

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,44 @@ const startButton = document.getElementById('start-btn');
 
 const questionContainerElement = document.getElementById('question-container')
 
+const questionElement = document.getElementById('question')
+
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+
+let shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame);
 
 
 function startGame() {
     console.log("Started");
     startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0 
     questionContainerElement.classList.remove('hide');
+    setNextQuestion()
 }
 
 
-function steNextQuestion() {
-
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
 
 function selectAnswer() {
     
 }
+
+const questions = [
+    {
+      question : "Which movie started the Marvel Universe?",
+      answer : [
+        { text : "Iron Man", correct: true},
+        { text : "Thor", correct: false}
+      ]   
+    }
+]
