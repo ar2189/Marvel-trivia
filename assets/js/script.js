249d1079232555e1ce407e6d0a363e23d63789bc
ar2189 <atif2189.ar@gmail.com>
2022-11-02 22:26:44 +0000

    Add const for start button and for question container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-function startGame() {
+const startButton = document.getElementById('start-btn');
 
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame);
+
+
+function startGame() {
+    console.log("Started");
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
 }
 
 
